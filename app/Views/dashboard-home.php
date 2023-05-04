ff4680313e5bea3f16c940b81f7552553f9465a1
Sipadil <fadileaja089@gmail.com>
2023-05-04 18:14:02 +0700

    lagi nyoba

--- app/Views/dashboard-home.php
+++ app/Views/dashboard-home.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <link rel="stylesheet" href="assets/css/styleutama.css">
+    <title>Document</title>
 </head>
 <body>
-    dashboard home
+    <div class="test">
+        DASHBOARD
+    </div>
 </body>
 </html>
