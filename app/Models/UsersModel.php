d0028356d3b9251d7c9239372f52bc8b3629f98c
Sipadil <112046344+Sipadil@users.noreply.github.com>
2023-05-05 22:44:35 +0700

    model for login validate

---
+++ app/Models/UsersModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsersModel extends Model
+{
+    protected $table      = 'users';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = ['username', 'password', 'cookie'];
+
+    public function getUsers()
+    {
+        return $this->findAll();
+    }
+}
