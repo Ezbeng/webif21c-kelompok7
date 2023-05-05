9918d602efa63341c87312d038d24ce5905a096a
Sipadil <112046344+Sipadil@users.noreply.github.com>
2023-05-05 22:39:04 +0700

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'hostname' => 'sql12.freemysqlhosting.net',
+        'username' => 'sql12616188',
+        'password' => 'LiZdBLsjWP',
+        'database' => 'sql12616188',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
