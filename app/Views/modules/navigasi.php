95bbcf8b03363bacc4ab998d86369978b83efe19
Sipadil <fadileaja089@gmail.com>
2023-05-05 13:20:06 +0700

    salah url

--- app/Views/modules/navigasi.php
+++ app/Views/modules/navigasi.php
@@ -12,7 +12,7 @@
     <div class="l-navbar" id="nav-bar">
       <nav class="nav">
         <div>
-          <a href="/dashboard-home" class="nav_logo">
+          <a href="dashboard-home" class="nav_logo">
             <img src="assets/images/UNIVERSITASTEKNOKRAT.png" width="40px" alt="">
             <span class="nav_logo-name">MANAGEMENT</span>
           </a>
@@ -58,7 +58,7 @@
             </a>
           </div>
         </div>
-        <a href="/login" class="nav_link logout-btn">
+        <a href="login" class="nav_link logout-btn">
           <i class='bx bx-log-out nav_icon'></i>
           <span class="nav_name">Log Out</span>
         </a>
