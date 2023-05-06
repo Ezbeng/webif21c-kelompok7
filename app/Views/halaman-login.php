461d5572def01dfee850ebd4f9c8bea2a67afe3e
Sipadil <fadileaja089@gmail.com>
2023-05-06 22:13:34 +0700

    update

--- app/Views/halaman-login.php
+++ app/Views/halaman-login.php
@@ -1,30 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styleutama.css">
-    <title>LOGIN</title>
 </head>
 <body>
-    <div class="container-form">
-        <form action="Home/login" class="form-case" method="POST">
-            <img src="assets/images/UNIVERSITASTEKNOKRAT.png" width="100px" alt="" class="avatar-login">
-            <label for="" class="title-form">SISTEM M.D.A TEKOKRAT</label>
-            <label for="">Username</label>
-            <input type="text" class="username" placeholder="Username" id="username" name="username">
-            <label for="">Password</label>
-            <input type="password" class="password" placeholder="Password" id="password" name="password">
-            <label>
-                <input type="checkbox">Ingat Password
-            </label>                  
-            <br>
-            <input type="submit" class="submit" value="LOGIN"> 
-            <?php if (session()->has('error')) : ?>
-              <div class="error"><?= session('error') ?></div>
-            <?php endif ?>               
-        </form>
+  <div class="container-fluid ps-md-0">
+    <div class="row g-0">
+      <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+      <div class="col-md-8 col-lg-6">
+        <div class="login-form d-flex align-items-center py-5">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-9 col-lg-8 mx-auto">
+
+                <div class="row align-items-center">
+                  <div class="col"></div>
+                  <div class="col-4">
+                  <img src="assets/images/UNIVERSITASTEKNOKRAT.png" class="avatar-tekno" alt="">
+                  </div>
+                  <div class="col"></div>
+                </div>
+                <br>
+                <h3 style="text-align:center; color: tomato">SISFO M.D.A TEKNOKRAT</h3>
+                <br>
+                <form action="Home/login" method="POST">
+                  <?php if (session()->has('error')) : ?>
+                      <div class="error"><?= session('error') ?></div>
+                  <?php endif ?>
+                  <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="username" name="username" placeholder="admin">
+                    <label for="floatingInput">Username</label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                  </div>
+
+                  <div class="form-check mb-3">
+                    <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
+                    <label class="form-check-label" for="rememberPasswordCheck">
+                      Ingat password
+                    </label>
+                  </div>
+
+                  <div class="d-grid">
+                    <button class="btn btn-lg btn-login text-uppercase fw-bold mb-2" type="submit">Log in</button>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
