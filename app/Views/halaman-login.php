e0e5b74abe8addf645dcbbdd5f8d84c8224a1ba0
Sipadil <fadileaja089@gmail.com>
2023-05-09 03:52:48 +0700

    salah

--- app/Views/halaman-login.php
+++ app/Views/halaman-login.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/styleutama.css">
     <title>LOGIN</title>
+    <link rel="stylesheet" href="./assets/css/styleutama.css">
 </head>
 <body style="background-image: url('/assets/images/Universitas_Teknokrat_Indonesia.jpg');">
     <div class="container-form">
