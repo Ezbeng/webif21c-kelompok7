3d5e5338b1776e40ca03d4d78ff9f4967338d051
Sipadil <112046344+Sipadil@users.noreply.github.com>
2023-05-09 04:59:09 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://webif21c-kelompok7.zya.me/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
